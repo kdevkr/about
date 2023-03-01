d147ebf684237fe7fc1f722d0a6d41acf3ee3ace
Mambo <kdevkr@gmail.com>
2023-03-01 21:30:59 +0900

    GA4 추가

--- about/.vitepress/config.js
+++ about/.vitepress/config.js
@@ -33,6 +33,20 @@ export default {
             },
         ],
         ["link", { rel: "shortcut icon", href: "./favicon/favicon.ico" }],
+        [
+            [
+                "script",
+                {
+                    async: true,
+                    src: "https://www.googletagmanager.com/gtag/js?id=G-V8LF04VMBF",
+                },
+            ],
+            [
+                "script",
+                {},
+                "window.dataLayer = window.dataLayer || [];\nfunction gtag(){dataLayer.push(arguments);}\ngtag('js', new Date());\ngtag('config', 'G-V8LF04VMBF');",
+            ],
+        ],
     ],
 
     // Theme related configurations.
